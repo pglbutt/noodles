734646057ef6e18afe1b0e3f9d2b38b71b79fc5c
Paul Glass <pnglass@gmail.com>
2015-06-13 04:50:47 +0000

    Add initial cli args with docopt

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,8 @@
+
+#![feature(plugin)]
+#![plugin(docopt_macros)]
+
+extern crate docopt;
+extern crate rustc_serialize;
+
 pub mod spag;
--- src/spag/main.rs
+++ src/spag/main.rs
@@ -1,3 +1,53 @@
+extern crate docopt;
+use docopt::Docopt;
+
+docopt!(Args derive Debug, "
+Usage: spag request [show] <file> [(-H <header>)...]
+       spag (get|post|put|patch|delete) <resource> [(-H <header>)...]
+       spag env set (<key> <val>)...
+       spag env show [<environment>]
+       spag history
+       spag history show <index>
+
+Options:
+    -h, --help      Show this message
+    -H, --header    Supply a header
+
+Arguments:
+    <resource>      The path of an api resource, like /v2/things
+    <header>        An http header, like 'Content-type: application/json'
+    <environment>   The name of an environment, like 'default'
+    <index>         An index, starting at zero
+");
+
 pub fn main() {
-    println!("poo from lib");
+    let args: Args = Args::docopt().decode().unwrap_or_else(|e| e.exit());
+    println!("{:?}", args);
+
+    if args.cmd_request {
+        spag_request(&args);
+    } else if args.cmd_history {
+        spag_history(&args);
+    } else if args.cmd_env {
+        spag_env(&args);
+    } else if args.cmd_get || args.cmd_post || args.cmd_put || args.cmd_patch || args.cmd_delete {
+        spag_method(&args);
+    }
 }
+
+fn spag_env(args: &Args) {
+    println!("called spag env");
+}
+
+fn spag_history(args: &Args) {
+    println!("called spag history");
+}
+
+fn spag_request(args: &Args) {
+    println!("called spag request");
+}
+
+fn spag_method(args: &Args) {
+    println!("called spag method");
+}
+
